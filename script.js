1b123bbd13c35df9e067e4b6fdf8c5fc7c458cf8
venpurai d <venpuravi@gmail.com>
2021-05-24 00:50:35 +0000

    script complete

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function compute()
-{
+{ // get values, basic validation and result publish
+    if(!checkInputValidity()){
+        return;
+    }
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
+    var mature=+principal + +interest; 
     var year = new Date().getFullYear()+parseInt(years);
-    
+    document.getElementById("result").innerText="If you deposit "+principal+",\nat an interest rate of "+rate+"%.\nYou will receive an amount of "+interest+",\nin the year "+year;    
 }
-function updateRateView(){
+
+
+function updateRateView(){ //update the view on setting the rate slider
   var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }  
+function checkInputValidity(){ // function to check principal amount to be empty,0 or negative
+    var principal = document.getElementById("principal").value;
+    //if(principal)
+    if(principal<=0){
+        document.getElementById("principal").focus();
+        window.alert("Enter a positive number");
+        
+        return false;
+    }
+    return true;
+}
 
         
